00a2813f144701bcb7c6b036505c0be6cc275e6c
shannon <liyunlong518@gmail.com>
2022-03-21 16:31:29 +0800

    click delete handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import './App.css'
 import React, { useState } from 'react'
 
 import Header from './components/Header'
-import { DeleteTask } from './components/Task'
+//import { DeleteTask } from './components/Task'
 import Tasks from './components/Tasks'
 import taskList from './db.json'
 
 function App() {
   const [tasks, setTasks] = useState(taskList.tasks)
 
-  const deleteTask: DeleteTask = (id: number) => {
-    console.log('delete: ', id)
-  }
-
   return (
     <div className="container">
       <Header title="Tasks Tracker" />
-      <Tasks tasks={taskList.tasks} onDelete={deleteTask} />
+      <Tasks
+        tasks={taskList.tasks}
+        onDelete={(id: number) => {
+          console.log('delete: ', id)
+        }}
+      />
     </div>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -8,14 +8,17 @@ type TaskItem = {
   reminder: boolean
 }
 
-type DeleteTask = (id: number) => void
+type DeleteTask = (id: number) => any
 
 function Task(param: { task: TaskItem; onDelete: DeleteTask }) {
   return (
     <div className="task">
       <h3>
         {param.task.text}
-        <FaTimes style={{ color: 'red', cursor: 'pointer' }} />
+        <FaTimes
+          style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => param.onDelete(param.task.id)}
+        />
       </h3>
       <p>{param.task.day}</p>
     </div>
